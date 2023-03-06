ff5a81509a1545e8c84c555d830dd48c4a0be63c
a_abd <a_abd@DESKTOP-85VTUKM>
2023-03-06 02:13:56 +0200

    Add project files.

---
+++ 004/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004
+{
+    public class Date
+    {
+        private int Year;
+        private int Month;
+        private int Day;
+
+        private readonly int []  Year365 =  {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        private readonly int [] Year366 =  { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+
+        public Date(int Year, int Month, int Day)
+        {
+            var isleap = Year % 4==1 &&(Year %100 !=0 || Year % 400==0 );
+            if (Year>=1&& Year <= 9999 && Month >= 1 && Month <= 12 && Day >=1 && Day <= 31)
+            {
+                int [] days = isleap ? Year365 : Year366;
+                this.Year = Year;
+                this.Month = Month;
+                this.Day = Day;
+            }
+            else
+            {
+                Console.WriteLine("NV");
+            }
+            
+            
+        }
+        // if user input only year
+        public Date(int Year) : this(Year , 01 ,01) 
+        {
+
+        }
+        public Date(int Year, int Month) : this(Year, Month, 01)
+        {
+
+        }
+
+        public string getDate() 
+        {
+            return $" {Day.ToString().PadLeft(2,'0')}/{Month.ToString().PadLeft(2,'0')}/{Year.ToString().PadLeft(4,'0')}";
+        }
+
+    }
+}
---
+++ 004/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Threading.Channels;
+using System.Xml.Schema;
+
+namespace _004
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Date d1 = new Date(2001,2,32);
+            Console.WriteLine(d1.getDate());
+            /// ones counter
+            //{
+            //    var counter = 0;
+            //    for (int i = 0; i < 1000000; i++)
+            //    {
+            //        string str = i.ToString();
+            //        counter += str.Count(c => c == '1');
+            //    }
+            //}
+
+
+
+        }
+       
+    }
+
+}   
